fced9e98756c537303b7c7bf119158f2949e6769
malipurva2006-gif <malipurva2006@gmail.com>
2026-08-13 15:48:57 +0530

    Time: 4 ms (13.97%), Space: 36.9 MB (14.87%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> sumleft(n);
+        vector<int> sumright(n);
+        sumleft[0]=0;
+        sumright[n-1]=0;
+        int ans=-1;
+        // construct sumleft array
+        for(int i =1;i<n;i++){
+            sumleft[i]=arr[i-1]+sumleft[i-1];
+        }
+        // construsting sumright array
+        for(int i =n-2;i>=0;i--){
+            sumright[i]=arr[i+1]+sumright[i+1];
+        }
+         for(int i =0;i<n;i++){
+            if(sumleft[i]==sumright[i]){
+                ans= i;
+                break;
+            }
+         }
+         return ans;
+    }
+};
